9e413a6772eb5b600c608781d8014f3e39596961
Matthew Kling <mattkling@gmail.com>
2016-01-20 14:08:45 -0800

    initialize user_parameters script and start changing references

---
+++ user_parameters.r
@@ -0,0 +1,9 @@
+
+# this script contains user-specific parameters referenced by the analysis scripts
+# it is in .gitignore, so your local changes will not be pushed
+
+project_stem_dir <- "E:/Phylo_modelling/"
+climate_data_dir <- "E:/BCM/CA_2014/Summary/HST/Normals_30years/"
+occurrence_data_dir <- "E:/Phylo_modelling/Data/Species"
+
+# etc
