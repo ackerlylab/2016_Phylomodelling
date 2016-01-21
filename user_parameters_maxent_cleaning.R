f7c0b993332c419d384b17c5c1a8b0ee30d9358e
naiamh <morueta-holme@berkeley.edu>
2016-01-21 12:31:55 -0800

    Updated original modelling script to reference user parameters, added file with parameters used in cleaning

---
+++ user_parameters_maxent_cleaning.R
@@ -0,0 +1,34 @@
+
+# this script contains user-specific parameters referenced by the analysis scripts
+# it is in .gitignore, so your local changes will not be pushed
+
+# directories
+
+# For lab computer
+project_stem_dir <- "E:/Phylo_modelling/"
+climate_data_dir <- "E:/BCM/CA_2014/Summary/HST/Normals_30years/"
+occurrence_data_dir <- paste0(project_stem_dir, "Data/Species")
+occurrence_data_dir_processed <- paste0(occurrence_data_dir, '/Processed/')
+maxent_output_dir <- "E:/Phylo_modelling/Output/Maxent"
+
+charts_output_dir <- "E:/Phylo_modelling/Output/Charts"
+
+# Version of maxent run
+maxent_run_version <- "V2"
+
+# species list file name
+species_list <- paste0(spdir, '0_Species_list.rdata')
+
+# occurrence background file name for maxent
+maxent_background <- paste(spdir, '10000_CA_plants_bg_1080m.rdata', sep='')
+
+# string for searching correct environmental data file names
+searchString <- '1080m'
+
+# number of nodes to use for parallel processing jobs
+# (shoud be no greater than number of processors in your machine)
+nodes <- 7
+
+# minimum number of records for maxent modeling
+maxent_min_records <- 10
+
