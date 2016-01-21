922d7dc4597fc34668a9eadb1fdcc2bc714d47eb
Matthew Kling <mattkling@gmail.com>
2016-01-20 19:27:43 -0800

    split parameters files into default and user, and add the later to gitignore -- master copy should remain shared/versioned

---
+++ default_parameters.r
@@ -0,0 +1,17 @@
+
+# this script contains user-specific parameters referenced by the analysis scripts
+# it is in .gitignore, so your local changes will not be pushed
+
+# directories
+project_stem_dir <- "E:/Phylo_modelling/"
+climate_data_dir <- "E:/BCM/CA_2014/Summary/HST/Normals_30years/"
+occurrence_data_dir <- "E:/Phylo_modelling/Data/Species"
+maxent_output_dir <- "E:/Phylo_modelling/Output/Maxent"
+charts_output_dir <- "E:/Phylo_modelling/Output/Charts"
+
+# number of nodes to use for parallel processing jobs
+# (shoud be no greater than number of processors in your machine)
+nodes <- 6
+
+# minimum number of records for maxent modeling
+maxent_min_records <- 10
