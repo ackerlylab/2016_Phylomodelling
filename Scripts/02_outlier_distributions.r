d1ec005d74d3a05495e077b096ec50265374192c
Matthew Kling <mattkling@gmail.com>
2016-01-20 19:01:42 -0800

    move and rename some files associated with previous 2 commits

--- Scripts/02_outlier_distributions.r
+++ Scripts/02_outlier_distributions.r
@@ -167,3 +167,20 @@ library(doParallel)
 results <- foreach(sp = unique(d$spp)) %do% { try(outlier_map(sp)) }
 #stopCluster(cl)
 
+
+
+### copy charts to second consolidated location
+
+infiles <- list.files("E:/Phylo_modelling/Output/Maxent/V2", pattern="outlier_map_v3.png", recursive=T, full.names=T)
+
+outdir <- "E:/Phylo_modelling/Output/Charts/Mapped_outliers"
+
+for(file in infiles){
+      dir <- dirname(file)
+      species <- substr(dirname(file), tail(gregexpr("/", dir)[[1]], 1)+1, 1000)
+      newfile <- paste0(outdir, "/", species, ".png")
+      if(file.exists(newfile)) next()
+      #writeLines(paste("moved", species))
+      file.copy(file, newfile)
+}
+
