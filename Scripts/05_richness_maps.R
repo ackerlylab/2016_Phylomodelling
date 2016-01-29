bacb7ab4d7252b5720013095d428b6f588c0fd25
ackerlylab <ackerlylab@gmail.com>
2016-01-28 16:40:57 -0800

    compairsons of thresholds and statistics using latest maxent and occurrence richness data

--- Scripts/05_richness_maps.R
+++ Scripts/05_richness_maps.R
@@ -143,52 +143,7 @@ for(min_cells in c(0,10,30)){ # create richness maps for each of the three thres
 
 
 
-
-############## comparison of thresholds 0, 10, 30 ####################
-
-r <- list.files(richness_dir, pattern="richness_810m", full.names=T)
-r <- stack(r)
-png(paste0(richness_dir, "/richness_thresholds_scatterplot_810m_maxent.png"), width = 800, height = 800)
-pairs(r)
-dev.off()
-
-v <- as.data.frame(rasterToPoints(r))
-v[,3:5] <- apply(v[,3:5], 2, scale)
-v$difference <- v$richness_810m_min30 - v$richness_810m_min0
-
-p <- ggplot(v, aes(x, y, fill=difference)) +
-        geom_raster() +
-        scale_fill_gradient2(min="darkred", mid="gray", high="darkblue", midpoint=0) +
-        coord_fixed(ratio=1) +
-        theme(panel.background=element_blank(), panel.grid=element_blank(),
-              axis.text=element_blank(), axis.title=element_blank(), axis.ticks=element_blank(),
-              legend.position="none", title=element_text(size=25)) +
-        guides(fill = guide_colorbar(barwidth=20)) +
-        labs(title="difference in relative richness,\nno minimum vs 30-cell minimum")
-h <- v %>%
-        mutate(lyr = plyr::round_any(difference, .1)) %>%
-        group_by(lyr) %>%
-        summarize(n=n())
-l <- ggplot(h, aes(lyr, n, fill=lyr)) + 
-        geom_bar(stat="identity", width=.1) + 
-        scale_fill_gradient2(min="darkred", mid="gray", high="darkblue", midpoint=0) +
-        theme(panel.background=element_blank(), panel.grid=element_blank(),
-              axis.text.y=element_blank(), axis.title.y=element_blank(), axis.ticks.y=element_blank(), legend.position="none") +
-        labs(x="z(min30) - z(min0)")
-
-png(paste0(richness_dir, "/richness_difference_30_0_810m_maxent.png"), width = 1200, height = 1500)
-plot(p)
-print(l, 
-      vp=viewport(x = .5, y = .6, 
-                  width = unit(0.4, "npc"), height = unit(0.25, "npc"),
-                  just = c("left", "bottom")))
-dev.off()
-
-
-
-
-############### comparison to occurrence density from David ###############
-
+################# upscale to 25 and 50 km ###############
 
 ### create 25k and 50k upscaled versions of binary range maps ###
 
@@ -231,8 +186,56 @@ for(min_cells in c(0,10,30)){
 
 
 
-### comparison of thresholds 0, 10, 30 in point richness data ###
+############## comparison of THRESHOLDS: 0, 10, 30 ####################
 
+### maxent layers ###
+for(res in c("25k", "50k", "810m")){
+        
+        r <- list.files(richness_dir, pattern=paste0("richness_", res), full.names=T)
+        r <- stack(r)
+        r <- mask(r, cali)
+        
+        png(paste0(richness_dir, "/richness_thresholds_scatterplot_", res, "_maxent.png"), width = 800, height = 800)
+        pairs(r)
+        dev.off()
+        
+        v <- as.data.frame(rasterToPoints(r))
+        v[,3:5] <- apply(v[,3:5], 2, scale)
+        v$difference <- v[,5] - v[,3]
+        
+        p <- ggplot(v, aes(x, y, fill=difference)) +
+                geom_raster() +
+                scale_fill_gradient2(min="darkred", mid="gray", high="darkblue", midpoint=0) +
+                coord_fixed(ratio=1) +
+                theme(panel.background=element_blank(), panel.grid=element_blank(),
+                      axis.text=element_blank(), axis.title=element_blank(), axis.ticks=element_blank(),
+                      legend.position="none", title=element_text(size=25)) +
+                guides(fill = guide_colorbar(barwidth=20)) +
+                labs(title=paste0("Difference in relative richness,\nno minimum vs 30-cell minimum,\n", res, " maxent"))
+        binwidth <- range(v$difference, na.rm=T)
+        binwidth <- (binwidth[2]-binwidth[1])/20
+        h <- v %>%
+                mutate(lyr = plyr::round_any(difference, binwidth)) %>%
+                group_by(lyr) %>%
+                summarize(n=n())
+        l <- ggplot(h, aes(lyr, n, fill=lyr)) + 
+                geom_bar(stat="identity", width=binwidth) + 
+                scale_fill_gradient2(min="darkred", mid="gray", high="darkblue", midpoint=0) +
+                theme(panel.background=element_blank(), panel.grid=element_blank(),
+                      axis.text.y=element_blank(), axis.title.y=element_blank(), axis.ticks.y=element_blank(), legend.position="none") +
+                labs(x="z(min30) - z(min0)")
+        
+        png(paste0(richness_dir, "/richness_difference_30_0_", res, "_maxent.png"), width = 1200, height = 1500)
+        plot(p)
+        print(l, 
+              vp=viewport(x = .5, y = .6, 
+                          width = unit(0.4, "npc"), height = unit(0.25, "npc"),
+                          just = c("left", "bottom")))
+        dev.off()
+}
+
+
+### and point richness layers ###
 for(res in c("25", "50")){
         for(stat in c("raw", "Rarefied")){
                 
@@ -259,7 +262,7 @@ for(res in c("25", "50")){
                               axis.text=element_blank(), axis.title=element_blank(), axis.ticks=element_blank(),
                               legend.position="none", title=element_text(size=25)) +
                         guides(fill = guide_colorbar(barwidth=20)) +
-                        labs(title=paste0("difference in relative richness,\nno minimum vs 30-cell minimum\n", res, "k ", stat))
+                        labs(title=paste0("difference in relative richness,\nno minimum vs 30-cell minimum,\n", res, "k ", stat))
                 binwidth <- range(v$difference, na.rm=T)
                 binwidth <- (binwidth[2]-binwidth[1])/20
                 h <- v %>%
@@ -287,83 +290,118 @@ for(res in c("25", "50")){
 
 
 
-### compare to occurrence density maps ###
+############## comparison of STATISTICS: maxent, raw, rarefied ####################
 
-for(res in c("25", "50")){
-        r <- stack(raster(paste0(richness_dir, "/richness_min", min_cells, "_", res, "k.tif")),
-                   stack(pr_files[grepl(paste0(res, "k"), pr_files)]))
-        names(r) <- c("maxent", "rarefied", "raw")
-        r <- mask(r, r[[3]])
-        #r <- mask(r, calc(r, sum))
-        r <- as.data.frame(rasterToPoints(r))
-        
-        s <- r %>%
-                gather(stat, value, -x, -y) 
-        
-        p1 <- ggplot(s, aes(x, y, fill=value)) + 
-                geom_raster() + 
-                facet_grid(.~stat) +
-                scale_fill_viridis(na.value="white")  +
-                coord_fixed(ratio=1) +
-                theme(panel.background=element_blank(), panel.grid=element_blank(),
-                      axis.text=element_blank(), axis.title=element_blank(), axis.ticks=element_blank(),
-                      legend.position="top", title=element_text(size=25)) +
-                guides(fill = guide_colorbar(barwidth=20)) +
-                labs(fill="species richness  ")
-        
-        t <- s %>%
-                group_by(stat) %>%
-                mutate(value = value/max(value, na.rm=T))
-        
-        p2 <- ggplot(t, aes(x, y, fill=value)) + 
-                geom_raster() + 
-                facet_grid(.~stat) +
-                scale_fill_viridis(na.value="white")  +
-                coord_fixed(ratio=1) +
-                theme(panel.background=element_blank(), panel.grid=element_blank(),
-                      axis.text=element_blank(), axis.title=element_blank(), axis.ticks=element_blank(),
-                      legend.position="top", title=element_text(size=25)) +
-                guides(fill = guide_colorbar(barwidth=20)) +
-                labs(fill="normalized richness (% of max)  ")
-        
-        u <- t %>%
-                spread(stat, value) %>%
-                mutate(maxent=maxent-maxent, rarefied=rarefied-maxent, raw=raw-maxent) %>%
-                gather(stat, value, -x, -y)
-        
-        p3 <- ggplot(u, aes(x, y, fill=value)) + 
-                geom_raster() + 
-                facet_grid(.~stat) +
-                scale_fill_gradientn(colours=c("darkblue", "dodgerblue", "gray", "orange", "darkred"),
-                                     limits=c(-1, 1), na.value="white")  +
-                coord_fixed(ratio=1) +
-                theme(panel.background=element_blank(), panel.grid=element_blank(),
-                      axis.text=element_blank(), axis.title=element_blank(), axis.ticks=element_blank(),
-                      legend.position="top", title=element_text(size=25)) +
-                guides(fill = guide_colorbar(barwidth=20)) +
-                labs(fill="difference from maxent (normalized)  ")
-        
-        p <- arrangeGrob(p1, p2, p3, ncol=1)
-        
-        png(paste0(paste0(richness_dir, "/richness_comparison_", res, ".png")), width=1000, height=1500)
-        grid.draw(p)
-        dev.off()
-        
-        png(paste0(paste0(richness_dir, "/richness_scatterplots_", res, ".png")), width=500, height=500)
-        pairs(r[c("maxent", "rarefied", "raw")])
-        dev.off()
+resolutions <- c(25, 50)
+thresholds <- c(0, 10, 30)
+
+for(res in resolutions){
+        for(thresh in thresholds){
+                
+                # load and stack rasters for the three stats
+                thresh_tag <- paste0("n", thresh); if(thresh==0) thresh_tag <- "all_bcm"
+                r <- list.files("C:/Lab_projects/2016_Phylomodelling/Data/Richness/point_richness_rasters", 
+                                pattern=thresh_tag, full.names=T)
+                r <- r[grepl(".grd", r) & grepl(paste0(res, "k"), r)]
+                m <- list.files(richness_dir, pattern=".tif", full.names=T)
+                m <- m[grepl(paste0(res, "k"), m) & grepl(paste0("min", thresh), m)]
+                s <- stack(c(r, m))
+                names(s) <- c("rarefied", "raw", "maxent")
+                s <- mask(s, s$raw) # mask maxent to CA domain
+                
+                # pairs plot
+                png(paste0(richness_dir, "/richness_statistics_scatterplot_", res, "k_min", thresh, ".png"), width = 800, height = 800)
+                pairs(s)
+                dev.off()
+                
+                # basic richness maps
+                s <- as.data.frame(rasterToPoints(s)) %>%
+                        gather(stat, value, -x, -y) %>%
+                        mutate(stat=factor(stat, levels=c("raw", "rarefied", "maxent")))
+                p1 <- ggplot(s, aes(x, y, fill=value)) + 
+                        geom_raster() + 
+                        facet_grid(.~stat) +
+                        scale_fill_viridis(na.value="white")  +
+                        coord_fixed(ratio=1) +
+                        theme(panel.background=element_blank(), panel.grid=element_blank(),
+                              axis.text=element_blank(), axis.title=element_blank(), axis.ticks=element_blank(),
+                              legend.position="top", title=element_text(size=25), strip.text=element_text(size=25)) +
+                        guides(fill = guide_colorbar(barwidth=20)) +
+                        labs(fill="richness (# spp)  ")
+                
+                # normalized richness maps
+                t <- s %>%
+                        group_by(stat) %>%
+                        mutate(value = scale(value))
+                #mutate(value = value/max(value, na.rm=T))
+                p2 <- ggplot(t, aes(x, y, fill=value)) + 
+                        geom_raster() + 
+                        facet_grid(.~stat) +
+                        scale_fill_viridis(na.value="white")  +
+                        coord_fixed(ratio=1) +
+                        theme(panel.background=element_blank(), panel.grid=element_blank(),
+                              axis.text=element_blank(), axis.title=element_blank(), axis.ticks=element_blank(),
+                              legend.position="top", title=element_text(size=25), strip.text=element_text(size=25)) +
+                        guides(fill = guide_colorbar(barwidth=20)) +
+                        labs(fill="normalized richness (z-score)  ")
+                
+                # differnce maps from maxent
+                u <- t %>%
+                        spread(stat, value) %>%
+                        mutate(rarefied=rarefied-maxent, raw=raw-maxent, maxent=maxent-maxent) %>%
+                        gather(stat, value, -x, -y)
+                mag <- max(abs(range(u$value, na.rm=T)))
+                p3 <- ggplot(u, aes(x, y, fill=value)) + 
+                        geom_raster() + 
+                        facet_grid(.~stat) +
+                        scale_fill_gradientn(colours=c("darkblue", "dodgerblue", "gray", "orange", "darkred"), 
+                                             na.value="white", limits=c(-mag, mag))  +
+                        coord_fixed(ratio=1) +
+                        theme(panel.background=element_blank(), panel.grid=element_blank(),
+                              axis.text=element_blank(), axis.title=element_blank(), axis.ticks=element_blank(),
+                              legend.position="top", title=element_text(size=25), strip.text=element_text(size=25)) +
+                        guides(fill = guide_colorbar(barwidth=20)) +
+                        labs(fill="difference from maxent (normalized)  ")
+                
+                # save combo plot
+                p <- arrangeGrob(textGrob(label=paste0("Species richness: statistics compared (", res, "km, ", "min ", thresh, " cells)"),
+                                          gp=gpar(fontsize=30)), 
+                                 p1, p2, p3, ncol=1, heights=c(.2, 1, 1, 1))
+                png(paste0(richness_dir, "/richness_comparison_", res, "k_min", thresh, ".png"), width=1000, height=1500)
+                grid.draw(p)
+                dev.off()
+                
+                
+                # difference from maxent as function of raw
+                z <- s %>%
+                        spread(stat, value) %>%
+                        gather(stat, value, rarefied, raw) %>%
+                        group_by(stat) %>%
+                        mutate(maxent_scaled=scale(maxent), value_scaled=scale(value),
+                               maxent_pmax=maxent/max(na.omit(maxent)), value_pmax=value/max(na.omit(value)),
+                               diff_basic=value-maxent,
+                               diff_scaled=value_scaled-maxent_scaled,
+                               diff_pmax=value_pmax-maxent_pmax) %>%
+                        select(x, y, stat, value, diff_basic:diff_pmax) %>%
+                        gather(diff_stat, diff_value, diff_basic:diff_pmax) %>%
+                        spread(stat, value)
+                        
+                plt <- ggplot(z, aes(raw, diff_value)) + 
+                        geom_point() +
+                        geom_smooth(se=F) +
+                        facet_grid(diff_stat~., scales="free") +
+                        theme_bw() +
+                        theme(legend.position="top") +
+                        labs(x="raw richness", y="",
+                             title=paste0(res, "km, ", "min ", thresh, " cells"))
+                ggsave(paste0(richness_dir, "/error_saturation_scatterplot_", res, "k_min", thresh, ".png"), plt, width=5, height=10, units="in")
+        }
 }
 
 
 
 
-
-
-
-
-
-
-######### species range sizes ############
+######### add range sizes to species table ############
 
 cl <- makeCluster(7)
 registerDoParallel(cl)
@@ -381,5 +419,16 @@ rd <- r[good]
 rd <- lapply(rd, as.vector)
 rd <- do.call("rbind", rd)
 rd <- cbind(basename(spp_dirs[good]), as.data.frame(rd))
-names(rd) <- c("species", "range_810m", "range_25km", "range_50km")
-write.csv(rd, paste0(richness_dir, "/range_size.csv"), row.names=F)
+names(rd) <- c("spp", "maxent810m", "maxent25km", "maxent50km")
+rd <- full_join(freqs, rd)
+rd <- select(rd, -X)
+rd <- select(rd, spp, nrecords, ncells, maxent810m, maxent25km, maxent50km)
+write.csv(rd, paste0(project_stem_dir, "/git_files/data/species_occurrence_counts.csv"), row.names=F)
+
+# pairs plots, linear and log scales
+png(paste0(richness_dir, "/species_range_scatterplot.png"), width=1000, height=1000)
+pairs(rd[,2:6], cex=.1)
+dev.off()
+png(paste0(richness_dir, "/species_range_scatterplot_loglog.png"), width=1000, height=1000)
+pairs(log10(rd[,2:6]), cex=.1)
+dev.off()
